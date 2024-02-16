bc7fabec4e2cd63517aa0a760d6a7d8122af5e88
Hayk Akunyan <haykak5@gmail.com>
2024-02-16 11:47:49 +0400

    Refactor ( Header Component ) Minor refactoring, remove unused lines of code.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 "use client";
+
 import Logo from "@/components/Logo";
 import ModeToggle from "@/components/ModeToggle";
 import GitHubLink from "@/components/GitHubLink";
@@ -13,16 +14,8 @@ import {
 } from "@/components/ui/sheet";
 
 import { Button } from "./ui/button";
-import { useState } from "react";
+
 const Header = () => {
-
-const [meals,setMeals] = useState(false)
-
-function showMeals(e){
-    e.preventDefault()
-    setMeals(true)
-    console.log('show meals')
-}
   return (
   <header className="header sticky top-0 z-40 w-full backdrop-blur shadow-md flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.5] bg-white/95 supports-backdrop-blur:bg-white/60 dark:shadow-none dark:bg-black/95">
       <div className="header__container container mx-auto px-4">
@@ -46,7 +39,7 @@ function showMeals(e){
             </Sheet>
 
             <Logo className={"w-16 mr-auto"} />
-            
+
             <div className="flex items-center gap-2">
                 <ModeToggle />
                 <GitHubLink />
