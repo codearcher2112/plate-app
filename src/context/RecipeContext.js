dfd413395fc09bc310a705cafc2b4a215393e667
Hayk Akunyan <haykak5@gmail.com>
2024-02-17 19:02:30 +0400

    Fix ( Recipe Item Author Avatar Image ) Fix each recipe item avatar image.

--- src/context/RecipeContext.js
+++ src/context/RecipeContext.js
@@ -4,7 +4,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 import { db, storage } from '@/firebase/firebase';
 import { collection, doc, addDoc, onSnapshot, deleteDoc } from 'firebase/firestore';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-
+import { UserAuth } from '@/context/AuthContext';
 const RecipeContext = createContext();
 
 export const useRecipeContext = () => {
@@ -13,18 +13,19 @@ export const useRecipeContext = () => {
 
 export const RecipeProvider = ({ children }) => {
     const [items, setItems] = useState([]);
-    const [newItem, setNewItem] = useState({ title: '', ingredients: '', instructions: '', imageUrl: '' });
+    const [newItem, setNewItem] = useState({ avatarUrl: '', title: '', ingredients: '', instructions: '', imageUrl: '' });
 
     const addItem = async () => {
         if (newItem.title !== '' && newItem.ingredients !== '' && newItem.instructions !== '' && newItem.imageUrl !== '') {
             await addDoc(collection(db, 'items'), {
+                avatarUrl: newItem.avatarUrl,
                 title: newItem.title.trim(),
                 ingredients: newItem.ingredients.trim(),
                 instructions: newItem.instructions.trim(),
                 imageUrl: newItem.imageUrl,
             });
 
-            setNewItem({ title: '', ingredients: '', instructions: '', imageUrl: '' });
+            setNewItem({ avatarUrl: '', title: '', ingredients: '', instructions: '', imageUrl: '' });
         }
     };
 
@@ -60,6 +61,14 @@ export const RecipeProvider = ({ children }) => {
             console.error('Error uploading file:', error);
         }
     };
+
+    const { user } = UserAuth();
+
+    useEffect(() => {
+        if (user) {
+            setNewItem((prevItem) => ({ ...prevItem, avatarUrl: user.photoURL }));
+        }
+    }, [user]);
 
     const contextValue = {
         items,
